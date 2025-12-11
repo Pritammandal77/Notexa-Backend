c5a896e2e7b267ca050262c5e9e8a51ee8b7cc35
Pritam Mandal <pritampmandal@gmail.com>
2025-12-11 15:21:37 +0530

    fixed the bug : I think so ..

--- src/config/passport.js
+++ src/config/passport.js
@@ -11,8 +11,8 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//   callbackURL: `${process.env.BACKEND_URL || 'http://localhost:8000'}/api/auth/google/callback`
-  callbackURL: "http://localhost:8000/api/auth/google/callback"
+  callbackURL: `${process.env.BACKEND_URL || 'http://localhost:8000'}/api/auth/google/callback`
+  // callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`
 
 }, async (accessToken, refreshToken, profile, done) => {
   try {

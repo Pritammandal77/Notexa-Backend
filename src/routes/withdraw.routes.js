77bf27b5bc857b448cbead15de99e11a58e44603
Pritam Mandal <pritampmandal@gmail.com>
2026-01-20 11:54:25 +0530

    solved some bugs

--- src/routes/withdraw.routes.js
+++ src/routes/withdraw.routes.js
@@ -10,7 +10,7 @@ withdrawRouter.route("/request-withdraw").post(verifyJWT, withdrawPayout)
 
 withdrawRouter.route("/all-withdraw-requests").get(verifyJWT, isAdmin, getAllWithDrawRequest)
 
-withdrawRouter.route("/update-withdraw-req-status").put(isAdmin, processWithDrawRequests)
+withdrawRouter.route("/update-withdraw-req-status").put(verifyJWT,isAdmin, processWithDrawRequests)
 
 withdrawRouter.route("/curr-user/withdraw-requests").get(verifyJWT, getCurrUserWithdrawReq)
 
